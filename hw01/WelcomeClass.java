b8b569a114c6f0eb4e0bc1118dca4baadd4ec2fc
lindseyjtaub <ljt219@lehigh.edu>
2015-08-28 17:55:00 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+///////////
+//Lindsey Taub
+//hw 01
+//Welcome Class Java
+
+public class WelcomeClass{
+    
+    public static void main(String[] args) {
+        
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-L--J--T--2--1--9->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Los Angeles born and raised");
+        
+    }
+}
